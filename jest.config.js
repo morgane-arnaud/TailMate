39c911e32e124d526a51e30444495284f9e93231
Morgane Arnaud <morgane@mohara.co>
2025-09-24 16:23:06 +0100

    Implement Jest and a test for the cat profile card

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'react-native',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  testMatch: ['**/__tests__/**/*.(ts|tsx|js)', '**/*.(test|spec).(ts|tsx|js)'],
+  moduleNameMapping: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+  ],
+  testEnvironment: 'node',
+};
